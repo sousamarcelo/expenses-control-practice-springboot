a539b34cd10ebbfe498f9046a9cfb8a935371917
Marcelo Sousa <marcelovsousa22@gmail.com>
2025-07-28 19:31:49 -0300

    Project started

---
+++ src/main/java/com/practice/expensescontrol/ExpensescontrolApplication.java
@@ -0,0 +1,13 @@
+package com.practice.expensescontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpensescontrolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpensescontrolApplication.class, args);
+	}
+
+}
