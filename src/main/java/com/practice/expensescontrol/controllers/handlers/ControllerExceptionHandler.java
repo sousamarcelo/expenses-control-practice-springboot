56f19d860513ea641eb305e16868560401720825
Marcelo Sousa <marcelovsousa22@gmail.com>
2025-07-29 22:51:30 -0300

    'ControllerExceptionHandler' created to handle exception in the controller layer request

---
+++ src/main/java/com/practice/expensescontrol/controllers/handlers/ControllerExceptionHandler.java
@@ -0,0 +1,33 @@
+package com.practice.expensescontrol.controllers.handlers;
+
+import java.time.Instant;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.practice.expensescontrol.dto.CustomError;
+import com.practice.expensescontrol.services.exceptions.DataBaseException;
+import com.practice.expensescontrol.services.exceptions.ResourceNotFoundException;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+	
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<CustomError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request){
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	@ExceptionHandler(DataBaseException.class)
+	public ResponseEntity<CustomError> dataBase(DataBaseException e, HttpServletRequest request){
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+
+}
--- src/main/java/com/practice/expensescontrol/services/ItemService.java
+++ src/main/java/com/practice/expensescontrol/services/ItemService.java
@@ -3,7 +3,6 @@ package com.practice.expensescontrol.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
